fa66b35ecf9aca4aafdc0c04679a351c5c62ae58
Adam Munoz <adam.makyn@gmail.com>
2017-02-03 02:23:08 +0900

    Start XML API

--- cgi-bin/api.py
+++ cgi-bin/api.py
@@ -1,4 +1,29 @@
+import sys
 import cgi
+import xml.etree.ElementTree as ET
 
-# Get url parameters
-arguments = cgi.FieldStorage()
+# constants
+
+XML_HEADER = 'Content-type: application/xml \r\n\r\n'
+
+# functions
+
+def resp(msg):
+  sys.stdout.write(msg)
+
+
+# data
+
+fields = cgi.FieldStorage()
+resource = fields['resource'].value
+
+# response
+
+x_amltpv_response = ET.Element('amltpv-response')
+x_resource = ET.SubElement(x_amltpv_response, 'resource')
+x_resource.text = resource
+
+# respond
+
+resp(XML_HEADER)
+resp(ET.tostring(x_amltpv_response))
--- start_server.py
+++
@@ -1,9 +0,0 @@
-import SocketServer
-import CGIHTTPServer 
-Handler = CGIHTTPServer.CGIHTTPRequestHandler
-PORT = 8000
-
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-
-print "serving at port", PORT
-httpd.serve_forever()
